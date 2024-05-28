ebfe714c4f8f9a8b01090fe34db9f8b660b3ce9b
Richter3766 <97567615+Richter3766@users.noreply.github.com>
2024-05-28 16:42:12 +0900

    Env: spring 초기 세팅

---
+++ src/test/java/com/example/highweb5back/HighWeb5BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.highweb5back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HighWeb5BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
